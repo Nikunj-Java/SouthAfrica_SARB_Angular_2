b0e67de111fca68a1608169894625c8f728f1d78
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2025-06-02 16:59:12 +0530

    Http Service App,Component Life Cycle

--- Day-1/HttpService/src/app/user-card/user-card.component.ts
+++ Day-1/HttpService/src/app/user-card/user-card.component.ts
@@ -1,4 +1,4 @@
-import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, ElementRef, Input, OnChanges, OnDestroy, OnInit, SimpleChanges, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-user-card',
@@ -12,7 +12,11 @@ AfterViewInit,AfterViewChecked,OnDestroy
 {
 
   @Input() name:string='';
+ 
   private intervalId:any;
+
+  counter =0;
+  oldName:string='';
 
   constructor(){
     console.log('[Constructor] component instance Created');
@@ -20,6 +24,11 @@ AfterViewInit,AfterViewChecked,OnDestroy
 
   ngOnChanges(changes: SimpleChanges): void {
     console.log('[ngOnChanges] input changed:',changes);
+    if(changes['name']){
+      const previous= changes['name'].previousValue;
+      const current=changes['name'].currentValue;
+      console.log(`Name Changed From "${previous}"  to "${current}"`);
+    }
   }
 
 
@@ -28,27 +37,39 @@ AfterViewInit,AfterViewChecked,OnDestroy
     this.intervalId= setInterval(()=>{
       console.log('[Interval] simulating background works...')
     },2000);
+
+    //start background task
+    this.intervalId=setInterval(()=>{
+      this.counter++;
+      console.log(`[Interval] Counter:${this.counter}`);
+    })
   }
   
   ngAfterViewChecked(): void {
-    console.log('[ngAfterViewChecked] content Checked!')
+   // console.log('[ngAfterViewChecked] content Checked!')
   }
   ngAfterViewInit(): void {
-    console.log('[ngAfterViewInit] View initilized!')
+    //console.log('[ngAfterViewInit] View initilized!')
   }
   ngAfterContentChecked(): void {
-    console.log('[ngAfterContentChecked] View checked!')
+    //console.log('[ngAfterContentChecked] View checked!')
   }
   ngAfterContentInit(): void {
-    console.log('[ngAfterContentInit]View Initialized!')
+   // console.log('[ngAfterContentInit]View Initialized!')
   }
+
   ngDoCheck(): void {
-    console.log('[ngDoCheck]Custom change detection logic running!')
+    //console.log('[ngDoCheck]Custom change detection logic running!')
+    if(this.oldName !== this.name){
+      console.log(`[ngDoCheck] Detected Name Chnage Via custom Logic: ${this.oldName} --> ${this.name}`);
+      this.oldName=this.name;
+    }
   }
  
   ngOnDestroy(): void {
      console.log('[ngOnDestroy]component being destroyed!')
      clearInterval(this.intervalId);
+     
   }
 
   
