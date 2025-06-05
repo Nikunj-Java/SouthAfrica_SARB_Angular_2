f64398af6260ed2c947b865433ac01389d554ca9
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2025-06-05 13:27:22 +0530

    myIF,SwitchMap-NgRx

--- Day-4/angular-app/src/app/app.component.ts
+++ Day-4/angular-app/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { UserService } from './service/user.service';
+import { FormControl } from '@angular/forms';
+import { debounce, debounceTime, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-app';
+
+  searchControl= new FormControl('');
+  users:any[]=[];
+  constructor(private service:UserService){
+    this.searchControl.valueChanges.pipe(
+      debounceTime(300),
+      switchMap(value=> this.service.serachUsers(value ?? ''))
+    ).subscribe(data=>this.users=data)
+    
+
+  }
+
 }
--- Day-4/angular-app/src/app/app.module.ts
+++ Day-4/angular-app/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DirectiveComponent } from './directive/directive.component';
 import { MyIfDirective } from './my-if-directive';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { MyIfDirective } from './my-if-directive';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
